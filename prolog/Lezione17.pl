c229eb6092e1abe4d9da400227c8db39d7c90a89
VittorioPorri <vittorioP18@gmail.com>
2024-05-16 14:14:26 +0200

    algo

---
+++ prolog/Lezione17.pl
@@ -0,0 +1,13 @@
+/*definire un predicato che possa riconoscere se data una grammatica la stringa appartiene al linguaggio*/
+
+'A'(L,R1):-
+  'B'(L,R),
+  'C'(R,R1).
+
+'A'(L,R2):-
+  'C'(L,R),
+  'B'(R,R1),
+  'B'(R1,R2).
+
+'B'([a|A],A).
+'C'([b|A],A).
