e588bea6174ef430659d4b9c4e80ec398c588b3c
VittorioPorri <VittorioP18@gmail.com>
2025-04-10 13:03:04 +0200

    js

---
+++ PW/esempi/es8-js/main.js
@@ -0,0 +1,28 @@
+// Questo è un commento 
+
+let myName = "lorenzo"; 
+
+/* 
+    Le variabili vanno dichiarate o con let o con var
+    Il tipo della variabile viene assegnato dinamicamente in base a cosa viene messo al suo interno
+*/
+
+console.log(myName.toUpperCase());  //stampo su console 
+
+/*
+    In javascript vengono introdotti i metodi sulla falsa riga di java
+    myName.toUpperCase()
+*/
+
+/*
+    Per aiutare le persone javascript ha creato diversi problemi ad esempio:
+    - "45" - 3 ritorna 42 
+    - "45" + 3 ritorna "453" poichè interpreta il "+" come concatenazione
+*/
+
+"use strict";
+/* 
+    Per fare in modo che non faccia supposizioni azzardate possiamo utilizzare la modalità in modo tale che 
+    mi riporta gli errori
+*/
+
